7eb18d0cb36eac48117106a8773c171ecb4ed2a5
Arman56rus <aisenov1995@gmail.com>
2021-08-12 03:06:43 +0500

    the task of 3week5day1part is ready on 80%!

---
+++ 3week5day/index.js
@@ -0,0 +1,93 @@
+// const searchTagClass = document.getElementsByClassName(`container`)
+// const searchTagClass1 = document.querySelector(`container`)
+//
+// const searchTagId = document.getElementById(`main_container`)
+// const searchTagId1 = document.querySelector(`main_container`)
+
+// const tagDivClassCont = document.getElementById("main_container1")
+//     tagDivClassCont.className = "container1"
+// const tagDivClassCont1 = document.getElementById("main_container1")
+// tagDivClassCont1.classList.add(("ok"))
+// tagDivClassCont1.classList.toggle(("you"))
+// tagDivClassCont1.classList.toggle(("ok"))
+// console.log(tagDivClassCont1.classList)
+// console.log(document.getElementById("main_container1"))
+// console.log(document.getElementsByClassName(`ok`))
+
+// const changeStyleDiv = document.getElementsByClassName(`pxWidth`)
+// changeStyleDiv[0].style.width = `200px`;
+// console.log(changeStyleDiv)
+//
+// const changeStyleDiv1 = document.getElementById(`pxWidth1`)
+// changeStyleDiv1.style.width = `300px`;
+// console.log(changeStyleDiv1)
+//
+// const addStyleList = document.getElementsByTagName(`ul`)
+// addStyleList[0].classList.add((`list`))
+// const addStyleListAddClass = document.getElementsByClassName(`list`)
+// console.log(addStyleListAddClass)
+// const addStyleListContent = addStyleListAddClass[0].innerHTML
+// console.log(addStyleListContent)
+// addStyleListAddClass[0].innerHTML =
+//     "            <li>one</li>\n" +
+//     "            <li class='point'>two</li>\n" +
+//     "            <li>three</li>\n" +
+//     "            <li class='point'>four</li>\n" +
+//     "            <li>five</li>\n" +
+//     "            <li class='point'>six</li>\n" +
+//     "            <li>seven</li>\n" +
+//     "            <li class='point'>eight</li>\n" +
+//     "            <li>nine</li>\n" +
+//     "            <li class='point'>ten</li>\n"
+// console.log(addStyleListAddClass[0].innerHTML)
+//
+// const newList = document.getElementsByClassName(`point`)
+// console.log(newList)
+// newList[0].style.cssText = `background: gray;
+//                              font-weight: bold;`;
+// newList[1].style.cssText = `background: gray;
+//                              font-weight: bold;`;
+// newList[2].style.cssText = `background: gray;
+//                              font-weight: bold;`;
+// newList[3].style.cssText = `background: gray;
+//                              font-weight: bold;`;
+// newList[4].style.cssText = `background: gray;
+//                              font-weight: bold;`;
+
+// const inputId = document.getElementById(`forFunc`)
+// console.log(inputId)
+//
+// const returnFunc =
+//     function returnFunc(param){
+//         return inputId
+//     }
+// returnFunc(inputId)
+
+// const inputClass = document.getElementsByClassName(`forFunc`)
+// console.log(inputClass)
+// const inputForFunc = inputClass[0].outerHTML = `<input type="text" class="forFunc">`
+//
+// const noActive =
+//     function noActiveFunc(param){
+//         if (param === "forFunc"){
+//             inputClass[0].outerHTML = `<input type="text" class="forFunc" disabled>`
+//         } else inputClass[0].outerHTML = `<input type="text" class="forFunc">`
+//     }
+//
+//     noActive("forFunc")
+
+// const textElement = document.getElementsByClassName(`addSpan`)
+// const newElement = document.createElement(`span`)
+// newElement.innerText = `First span`
+// textElement[0].prepend(newElement);
+// textElement[0].insertAdjacentHTML(`beforeend`, `<span>Second span</span>`)
+//
+// const searchSpan = document.getElementById(`forFuncSpan`)
+// const textElementContentText = searchSpan.textContent
+// const textElementContentInner = searchSpan.innerHTML = `forFuncSpan`
+//
+// function getParamSpan (param1, param2){
+//     console.log("Parameters were got: ", {textElementContentInner, textElementContentText})
+// }
+//
+// getParamSpan({textElementContentText},{textElementContentInner})
