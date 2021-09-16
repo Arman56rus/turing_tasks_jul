c4141e719a8ebece86fe421dd5bccdd04ce5f13a
Arman56rus <aisenov1995@gmail.com>
2021-09-16 21:40:32 +0500

    Упражнение 16 JS исправлено!

--- 4week2dayJS/index.js
+++ 4week2dayJS/index.js
@@ -5,9 +5,9 @@ class Employee{
     #hours;
     constructor(name, age, salaryPerHour, hours) {
         this.name = name
-        this.#age = age
-        this.#salaryPerHour = salaryPerHour
-        this.#hours = hours
+        this.age = age
+        this.salaryPerHour = salaryPerHour
+        this.hours = hours
     }
     set dataUser(value){
         [this.#name, this.#age, this.#salaryPerHour, this.#hours] = value.split("")
@@ -15,86 +15,91 @@ class Employee{
     get dataUser(){
         return  `${this.#name}, ${this.#age}, ${this.#salaryPerHour}, ${this.#hours}`
     }
-    // getSalary(){
-    //     console.log(`name: `+ this.name + `,` + ` salary: ` + this.salaryPerHour * this.hours)
-    // }
-
+    getSalary(){
+        console.log(`name: `+ this.name + `,` + ` salary: ` + this.salaryPerHour * this.hours)
+    }
     getDataEmployee(){
-        console.log(`Data: ` + this.#name + `, ` + this.#age + `, ` + this.#salaryPerHour * this.#hours)
+        console.log(`Data: ` + this.name + `, ` + this.age + `, ` + this.salaryPerHour * this.hours)
     }
 
 }
-// const employeeFirst = new Employee(`John`,32,10,7)
-// const employeeSecond = new Employee(`Kate`,30,12,7)
-// employeeFirst.getSalary()
-// employeeSecond.getSalary()
-// employeeFirst.getDataEmployee()
+const employeeFirst = new Employee(`John`,32,10,7)
+const employeeSecond = new Employee(`Kate`,30,12,7)
+employeeFirst.getSalary()
+employeeSecond.getSalary()
+employeeFirst.getDataEmployee()
 
-// class Numbers{
-//     constructor(...number) {
+// class Numbers {
+//     constructor(number) {
 //         this.number = number
 //     }
-//     set number(value){
-//        value.map((elem) => elem%2 === 0 ? console.log(`Число четное`) : this._number = elem)
+//
+//     evenNumber() {
+//         if (this.number % 2 === 0) {
+//             return ('число четное')
+//         } else {
+//             return ('число нечетное')
+//         }
 //     }
-//     set number(value){
-//        value.map((elem) => elem%1 === 0 ? console.log(`Число целое`) : this._number = elem)
+//
+//     integerNumber() {
+//         if (this.number % 1 === 0) {
+//             return ('число целое')
+//         } else {
+//             return ('число с плавающей точкой')
+//         }
 //     }
-//     set number(value){
-//        value.map((elem) => elem>0 ? console.log(`Число положительное`) : elem === 0 ? console.log(`Число ноль`) : console.log(`Число отрицательное`))
+//
+//     positiveNumber() {
+//         if (this.number > 0) {
+//             return ('число положительное')
+//         } else if (this.number === 0) {
+//             return ('число ноль')
+//         } else {
+//             return ('число отрицательное')
+//         }
 //     }
-//     get number(){
-//         return this._number
-//     }
-//     showNumber(){
-//         console.log(`Numbers: ${this.number}`)
+//
+//     allFuncSummon() {
+//         console.log(
+//             this.evenNumber(),
+//             this.integerNumber(),
+//             this.positiveNumber()
+//         )
 //     }
 // }
 //
-// const objNumOne = new Numbers(12,13,14,15.4, -2,0)
-// objNumOne.showNumber()
+// const firstObj = new Numbers(-4.6)
+// console.log(firstObj.allFuncSummon())
 
-class Strings{
-    constructor(...string) {
-        this.string = string
-    }
-    set string(value){
-        const firstLetter = value[0].split(``)
-        const upperCaseFirstLetter = firstLetter.map(function (firstLetter){
-            return firstLetter.toUpperCase()
-            //// не знаю, как сделать
-            // заглавной первую букву в каждом слове через цикл for ()
-        })
-        // const deleteFirstLetter = firstLetter.splice(0,1)
-        // firstLetter.splice(0,0,upperCaseFirstLetter)
-        const toStringFirstLetter = upperCaseFirstLetter.join(``)
-        value = toStringFirstLetter
-        this._string = value
-    }
-    set string(value){
-        const firstLetter = value[0].split(``)
-        const upperCaseFirstLetter = firstLetter.map(function (firstLetter){
-            return firstLetter.toLowerCase()
-        })
-        // const deleteFirstLetter = firstLetter.splice(0,1)
-        // firstLetter.splice(0,0,upperCaseFirstLetter)
-        const toStringFirstLetter = upperCaseFirstLetter.join(``)
-        value = toStringFirstLetter
-        this._string = value
-    }
-    get string(){
-        return this._string
-    }
-    changeString(){
-        console.log(`String: ${this.string}`)
-    }
-}
-const objString = new Strings("i love you")
-const objString1 = new Strings(`I LOVE YOU`)
-objString.changeString()
-objString1.changeString()
+
+// class Strings{
+//     constructor(string) {
+//         this.string = string
+//     }
+//
+//     toUpperWord(){
+//         return this.string.split(/\s/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
+//     }
+//     toLowerWord(){
+//         return this.string.split(/\s/).map(word => word[0].toLowerCase() + word.substring(1)).join(' ')
+//     }
+//     lengthWord(){
+//         // console.log(this.string.split(/\s/).join('').length)
+//         return this.string.split(/\s/).join('').length
+//     }
+//
+// }
+// const objString = new Strings("I Love You to")
+// console.log(objString.lengthWord())
+
+
+
 
 class AccountingEmployee extends Employee{
+    constructor(name, age, salaryPerHour, hours) {
+        super(name, age, salaryPerHour, hours);
+    }
     work(){
         console.log(`${this.name} приступил к выполнению обязанностей`)
     }
